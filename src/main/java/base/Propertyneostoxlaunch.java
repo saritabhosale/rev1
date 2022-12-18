6f221e98c946d7cc42e714c9d9869ef1af98088c
sarita <saritabhosale11.instru@gmail.com>
2022-12-17 19:41:31 -0800

    second comment for genkin in property file

--- src/main/java/base/Propertyneostoxlaunch.java
+++ src/main/java/base/Propertyneostoxlaunch.java
@@ -23,6 +23,7 @@ public class Propertyneostoxlaunch
 		
 		//launch browser by using property file
 		//i add comment for genkin
+		//second comment for genkin run
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Desktop\\chrome1\\chromedriver.exe");
 		driver =new ChromeDriver();
 		driver.get(Propertyfilestudy.readdatafrompropertyfile("url"));
